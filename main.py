b68df8d5a7113f8e8e5bb9143983b0f9453e493e
Hacene Tchoketch-kebir <hassentchoketch@gmail.com>
2025-12-25 14:10:11 +0100

    Instantiate config object in main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-if __name__ == '__main__':
-    print('GPT-2 Project Initialized!')
+from utils.config import ExperimentConfig
+
+
+config = ExperimentConfig.load("configs/gpt2_small.yaml")
+
+if __name__ == "__main__":
+    print("GPT-2 Project Initialized!")
