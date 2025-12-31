8a82962392922aa3c94ac9a205ae620dd5765cbf
Hacene Tchoketch-kebir <hassentchoketch@gmail.com>
2025-12-31 15:29:21 +0100

    update src/utils/checkpoint.py

--- src/utils/checkpoint.py
+++ src/utils/checkpoint.py
@@ -2,19 +2,31 @@ import os
 import torch
 
 
-def save_checkpoint(model, optimizer, step, loss, checkpoints_dir="checkpoints"):
+def save_checkpoint(
+    model, optimizer, step, val_loss, checkpoints_dir="checkpoints", is_best=False
+):
+    """
+    Saves the model state. Overwrites 'latest' always,
+    and 'best' only if is_best is True.
+    """
     if not os.path.exists(checkpoints_dir):
-        os.chdir(checkpoints_dir)
+        os.makedirs(checkpoints_dir)
 
     checkpoint = {
         "step": step,
         "model_state_dict": model.state_dict(),
         "optimizer_state_dict": optimizer.state_dict,
-        "loss": loss,
+        "loss": val_loss,
     }
-    path = os.path.join(checkpoints_dir, f"chpt_step_{step}.pt")
-    torch.save(checkpoint, path)
-    print(f"Checkpoint saves at step {step}")
+    # 1. Always save as the 'latest' for recovery
+    latest_path = os.path.join("checkpoints", "latest_ckpt.pt")
+    torch.save(checkpoint, latest_path)
+
+    # 2. Only overwrite 'best_model' if it's actually the best
+    if is_best:
+        best_path = os.path.join("checkpoints", "best_model.pt")
+        torch.save(checkpoint, best_path)
+        print(f"🏆 New best model saved! (Validation Loss: {val_loss:.4f})")
 
 
 def load_checkpoint(path, model, optimizer):
